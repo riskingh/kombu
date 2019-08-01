ce8b113112dca122c9e27a240980d6380a597ca6
Maxim Grishkin <riskingh@yandex-team.ru>
2019-08-01 13:12:42 +0300

    mongo connection string fix

--- kombu/utils/url.py
+++ kombu/utils/url.py
@@ -14,14 +14,23 @@ from kombu.five import string_t
 safequote = partial(quote, safe='')
 
 
+# https://github.com/celery/kombu/commit/cbf4fd6afd613da5ee9c8b50d5ad0d905563faeb
 def _parse_url(url):
     scheme = urlparse(url).scheme
     schemeless = url[len(scheme) + 3:]
     # parse with HTTP URL semantics
-    parts = urlparse('http://' + schemeless)
+    parts = urlparse("http://" + schemeless)
+
+    # The first pymongo.Connection() argument (host) can be
+    # a mongodb connection URI. If this is the case, don't
+    # use port but let pymongo get the port(s) from the URI instead.
+    # This enables the use of replica sets and sharding.
+    # See pymongo.Connection() for more info.
+    port = scheme != 'mongodb' and parts.port or None
+    hostname = schemeless if scheme == 'mongodb' else parts.hostname
     path = parts.path or ''
     path = path[1:] if path and path[0] == '/' else path
-    return (scheme, unquote(parts.hostname or '') or None, parts.port,
+    return (scheme, unquote(hostname or '') or None, port,
             unquote(parts.username or '') or None,
             unquote(parts.password or '') or None,
             unquote(path or '') or None,
